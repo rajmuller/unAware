582edda2d374af5a64d264db21b46477cf3587e0
Adam Rajmuller <adam.rajmuller@doclerholding.com>
2020-07-14 14:43:48 +0200

    types of store/modal

--- frontend/store/selectors.ts
+++ frontend/store/selectors.ts
@@ -1,1 +1,10 @@
-export const selectModal = (state) => state.modal;
+import { createSelector } from "reselect";
+import { ModalState } from "./slices/modal";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const modalSelector = (state: any): ModalState => state.modal;
+
+export const activeModalSelector = createSelector(
+  modalSelector,
+  (state) => state.active
+);
--- frontend/store/slices/modal.ts
+++ frontend/store/slices/modal.ts
@@ -1,15 +1,22 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export type ModalState = {
-  active: string;
+  active: string | null;
+};
+
+const initialState: ModalState = {
+  active: null,
 };
 
 const modal = createSlice({
   name: "modal",
-  initialState: null,
+  initialState,
   reducers: {
-    changeActive: (state: ModalState, action: PayloadAction<ModalState>) => {
+    changeActive: (state: ModalState, action: PayloadAction<string>) => {
       return { ...state, active: action.payload };
+    },
+    dismiss: (state: ModalState) => {
+      return { ...state, active: null };
     },
   },
 });
